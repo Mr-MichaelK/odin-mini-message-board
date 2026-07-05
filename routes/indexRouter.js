cc7bb0df0801be2485c87e5754f0c0d899641500
Mr-MichaelK <michael1kolanjian2@gmail.com>
2026-07-05 11:35:57 +0300

    Remove redundant messages array

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,19 +7,6 @@ const {
 } = require("../controllers/indexController");
 const indexRouter = Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
 indexRouter.get("/", getMessages);
 
 indexRouter.get("/new", getNewMessageForm);
