f87c3e0bffc648429b449db2497da9365a91cbe5
Mr-MichaelK <michael1kolanjian2@gmail.com>
2026-07-05 10:05:25 +0300

    Build the home messageboard route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) => {
-  res.send("Home screen which displays all messages");
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 indexRouter.get("/new", (req, res) => {
