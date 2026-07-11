e743bf999a8ee99abe45178fd00568a7f136b657
Mr-MichaelK <michael1kolanjian2@gmail.com>
2026-07-11 23:57:03 +0300

    Remove localhost url in console log

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ app.use("/", indexRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
